e0abf75084ac5c88e7103972873f570b4151d68b
AhmedAljamal <gad59125@gmail.com>
2025-08-21 16:30:44 +0300

    firebase android setup-done

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,14 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+// توحيد مجلدات الـ build (اختياري)
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+    val subBuild = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(subBuild)
     project.evaluationDependsOn(":app")
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gadoo_store/gadoo_store_app.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+  ?await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyAnvs1Ohez9-xRyBglBHB8KG4lzE_lQncw", 
+      appId: "1:484482921697:android:3af2a8b8c526e7692b33a0", 
+      messagingSenderId: "484482921697", 
+      projectId: "gadoostore-608c3",
+      ),
+  )
+
+
+
+
+
+  :await Firebase.initializeApp();
   runApp( const GadooStore());
 }
 
